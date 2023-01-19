9d7f76baf3952532d100a0d718fd814924831020
dbianchii <gdbianchii@gmail.com>
2023-01-19 20:25:51 -0300

    Interface, uuid

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,38 +1,12 @@
-import { useState } from "react"
 import Item from "./Item"
 import style from "./lista.module.scss"
 
-function Lista() {
-	const [tarefas, setTarefas] = useState([
-		{
-			tarefa: "React",
-			tempo: "02:00:00",
-		},
-		{
-			tarefa: "Javascript",
-			tempo: "01:00:00",
-		},
-		{
-			tarefa: "Typescript",
-			tempo: "01:00:00",
-		},
-	])
+import { ITarefa } from "../../types/tarefa"
 
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
 	return (
 		<aside className={style.listaTarefas}>
-			<h2
-				onClick={() => {
-					setTarefas([
-						...tarefas,
-						{
-							tarefa: "Estudar estado",
-							tempo: "05:00:00",
-						},
-					])
-				}}
-			>
-				Estudos do Dia
-			</h2>
+			<h2>Estudos do Dia</h2>
 			<ul>
 				{tarefas.map((item, i) => (
 					<Item key={i} {...item} />
--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,17 +1,45 @@
 import React from "react"
+import { ITarefa } from "../../types/tarefa"
 import Button from "../Button"
 import style from "./formulario.module.scss"
+import { v4 as uuidv4 } from "uuid"
+// This is an example of a class component. It is an old way of writing components.
+// It is still used, but it is not recommended to use it anymore.
+// The new way of writing components is using functions.
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+	setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
+	state = {
+		tarefa: "",
+		tempo: "00:00:00",
+	}
+
+	adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+		this.props.setTarefas((tarefasAntigas) => [
+			...tarefasAntigas,
+			{ ...this.state, selecionado: false, completado: false, id: uuidv4() },
+		])
+		this.setState({ tarefa: "", tempo: "00:00:00" })
+	}
+
 	render() {
 		return (
-			<form className={style.novaTarefa}>
+			<form
+				className={style.novaTarefa}
+				onSubmit={this.adicionarTarefa.bind(this)}
+			>
 				<div className={style.inputContainer}>
 					<label htmlFor="tarefa">Adicione um novo estudo</label>
 					<input
 						type="text"
 						name="tarefa"
 						id="tarefa"
+						value={this.state.tarefa}
+						onChange={(e) => {
+							this.setState({ ...this.state, tarefa: e.target.value })
+						}}
 						placeholder="O que você quer estudar?"
 					/>
 				</div>
@@ -21,12 +49,16 @@ class Formulario extends React.Component {
 						type="time"
 						step="1"
 						name="tempo"
+						value={this.state.tempo}
+						onChange={(e) => {
+							this.setState({ ...this.state, tempo: e.target.value })
+						}}
 						id="tempo"
 						min="00:00:00"
 						max="01:30:00"
 					/>
 				</div>
-				<Button>Button</Button>
+				<Button type="submit">Button</Button>
 			</form>
 		)
 	}
